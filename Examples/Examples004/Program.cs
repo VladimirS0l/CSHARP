eabc2dcd1c7350978caf272e96a737b7d4a65832
SolarevVladimir <vbnm1993@yandex.ru>
2022-06-22 22:47:28 +0500

    Добавили деление 2 варианта

--- Examples/Examples004/Program.cs
+++ Examples/Examples004/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//int numberA = 36;
+//int numberB = 6;
+//Console.WriteLine(numberA / numberB);
+
+double numberA = 38;
+double numberB = 6;
+Console.WriteLine(numberA / numberB);
