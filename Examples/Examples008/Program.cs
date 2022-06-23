0381eb103a0ffb80a373314cf1c23ed8f9ca1071
SolarevVladimir <vbnm1993@yandex.ru>
2022-06-23 21:16:16 +0500

    fish

--- Examples/Examples008/Program.cs
+++ Examples/Examples008/Program.cs
@@ -1,1 +1,31 @@
 ﻿
+int count = 0;
+int distance = 10000;
+int firstSpeedF = 1;
+int secondSpeedF = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time;
+
+if(distance > 10)
+{
+    if(friend == 1)
+    {
+        time = distance / (firstSpeedF + dogSpeed);
+        friend = 2;
+        distance = distance - (firstSpeedF + secondSpeedF) * time;
+        count += 1;
+    }
+
+    else
+    {
+        time = distance / (secondSpeedF + dogSpeed);
+        friend = 1;
+        distance = distance - (firstSpeedF + secondSpeedF) * time;
+        count += 1;
+    }
+}
+
+Console.Write("Собака пробежала ");
+Console.Write(count);
+Console.WriteLine(" раз");
